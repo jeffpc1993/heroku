7774888623b895bcb98afcc0f9ab8a8ae769a403
Jeff PC <jeff.chacko@myntra.com>
2015-11-17 18:42:04 +0530

    kjsdfha`:

--- httpserver.js
+++ httpserver.js
@@ -1,8 +1,8 @@
 var http=require('http');
-const PORT=8080;
+const PORT=5000;
 
 function handleRequest(request,response){
-  response.writeHead(200, {"Content-Type": "application/json",'Access-Control-Allow-Origin':'http://localhost:8000'});
+  response.writeHead(200, {"Content-Type": "application/json",'Access-Control-Allow-Origin':'null'});
   var json=JSON.stringify({imagesize:150,correct:[2,4],gridtype:"4x4",images:["http://lorempixel.com/150/150"]});
   response.end(json);
 }
